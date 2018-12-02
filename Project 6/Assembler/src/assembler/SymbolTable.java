964d62601c4845ef3a2d31201b4dbb437c3af4ea
Sophie Garrett <sarasnake99@live.com>
2018-12-01 22:02:10 -0500

    add symbol table

--- Project 6/Assembler/src/assembler/SymbolTable.java
+++ Project 6/Assembler/src/assembler/SymbolTable.java
@@ -1,8 +1,41 @@
 package assembler;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SymbolTable {
     
+    HashMap<String, Integer> symbols;
+    
     public SymbolTable() {
+        symbols = new HashMap<>();
+        this.addDefaults();
+        System.out.println(symbols);
+    }
+    
+    public void addEntry(String symbol, int address) {
+        symbols.put(symbol, address);
+    }
+    
+    public boolean contains(String symbol) {
+        return symbols.containsKey(symbol);
+    }
+    
+    public int getAddress(String symbol) {
+        return symbols.get(symbol);
+    }
+    
+    void addDefaults() {
+        addEntry("SP", 0);
+        addEntry("LCL", 1);
+        addEntry("ARG", 2);
+        addEntry("THIS", 3);
+        addEntry("THAT", 4);
         
+        for (int i = 0; i <= 15; i++) {
+            addEntry(("R" + i), i);
+        }
+        
+        addEntry("SCREEN", 16384);
+        addEntry("KBD", 24576);
     }
 }
